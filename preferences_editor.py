3a289c32bd80f7d1a761c37ac6e8254dbf6be931
phrack <project.shootoff@gmail.com>
2013-10-26 12:29:40 -0400

    Added missing copyright notice

--- preferences_editor.py
+++ preferences_editor.py
@@ -1,3 +1,7 @@
+# Copyright (c) 2013 phrack. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
 import Tkinter
 
 DETECTION_RATE = "detectionrate" #ms
