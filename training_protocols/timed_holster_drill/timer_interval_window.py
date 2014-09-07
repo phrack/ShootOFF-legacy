f4d3beb0f2abbd688c0df984508b9c378e732ac1
phrack <code.realm@gmail.com>
2014-09-07 08:21:43 -0400

    Min and max spinboxes now have default values.

--- training_protocols/timed_holster_drill/timer_interval_window.py
+++ training_protocols/timed_holster_drill/timer_interval_window.py
@@ -2,11 +2,18 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import Tkinter, ttk
+import Tkinter, tkMessageBox, ttk
 
 class TimerIntervalWindow():
     def _ok_click(self):
-        self._notify_interval(int(self._min_spinbox.get()), int(self._max_spinbox.get()))     
+        min_val = int(self._min_spinbox.get())
+        max_val = int(self._max_spinbox.get())
+
+        if (max_val <= min_val):
+            tkMessageBox.showerror("Bad Delay Interval", "Max must be greater than min.")
+            return
+
+        self._notify_interval(min_val, max_val)     
         self._window.destroy()   
 
     def build_gui(self, parent):
@@ -20,6 +27,8 @@ class TimerIntervalWindow():
         min_label = Tkinter.Label(self._window, text="Min (s)") 
         min_label.grid(row=0, column=0)
         self._min_spinbox = Tkinter.Spinbox(self._window, from_=1, to=300)
+        self._min_spinbox.delete(0, "end")
+        self._min_spinbox.insert(0, 4)
         interval_validator = (self._window.register(self.check_interval),'%P')
         self._min_spinbox.config(validate="key", validatecommand=interval_validator)
 
@@ -28,6 +37,8 @@ class TimerIntervalWindow():
         max_label = Tkinter.Label(self._window, text="Max (s)")	
         max_label.grid(row=1, column=0)
         self._max_spinbox = Tkinter.Spinbox(self._window, from_=1, to=300)
+        self._max_spinbox.delete(0, "end")
+        self._max_spinbox.insert(0, 8)
         self._max_spinbox.config(validate="key", validatecommand=interval_validator)
         self._max_spinbox.grid(row=1, column=1)
 
