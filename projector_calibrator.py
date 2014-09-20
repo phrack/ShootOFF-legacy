d4440392e00455ac103ebf17aec617a7a6255277
phrack <project.shootoff@gmail.com>
2014-09-20 17:34:13 -0400

    Fixed two bugs in calibrator: max_coord didn't work route (now finds farthest coord) and the bounding box was being drawn even if we didn't find both shapes.

--- projector_calibrator.py
+++ projector_calibrator.py
@@ -3,6 +3,7 @@
 # found in the LICENSE file.
 
 import cv2
+import math
 
 class ProjectorCalibrator():
     def get_projected_bbox(self):
@@ -10,7 +11,7 @@ class ProjectorCalibrator():
 
     def calibrate_projector(self, webcam_image):
         bw = cv2.cvtColor(webcam_image, cv2.cv.CV_BGR2GRAY)
-        (thresh, bw_image) = cv2.threshold(bw, 200, 255, cv2.THRESH_BINARY)
+        (thresh, bw_image) = cv2.threshold(bw, 100, 255, cv2.THRESH_BINARY)
         contours,h = cv2.findContours(bw_image, cv2.cv.CV_RETR_EXTERNAL,
                         cv2.cv.CV_CHAIN_APPROX_SIMPLE)
 
@@ -19,11 +20,16 @@ class ProjectorCalibrator():
         self._bottom_x = 0
         self._bottom_y = 0
 
+        detected_triangle = False
+        detected_rectangle = False
+
         for cnt in contours:
             approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
 
             # Detect green triangle in top left corner
             if len(approx) == 3:
+                detected_triangle = True
+    
                 self._top_x, self._top_y = self.far_left_coord(approx)
 
                 cv2.drawContours(webcam_image, [approx], 0, (0,255,0), -1)
@@ -32,14 +38,17 @@ class ProjectorCalibrator():
 
             # Detect red rectangle in bottom right corner
             elif len(approx) == 4:
-                self._bottom_x, self._bottom_y = self.max_coord(approx)
+                detected_rectangle = True
+                self._bottom_x, self._bottom_y = self.farthest_coord(approx)
 
                 cv2.drawContours(webcam_image, [approx], 0, (255,0,0), -1)
                 cv2.circle(webcam_image, (self._bottom_x, self._bottom_y), 
                     10, (0,255,0))
 
-        cv2.rectangle(webcam_image, (self._top_x, self._top_y), 
-            (self._bottom_x, self._bottom_y), (255,255,0))
+        # Draw bounding box for projector arena
+        if detected_triangle and detected_rectangle:
+            cv2.rectangle(webcam_image, (self._top_x, self._top_y), 
+                (self._bottom_x, self._bottom_y), (255,255,0))
 
         return webcam_image
 
@@ -54,18 +63,24 @@ class ProjectorCalibrator():
 
         return far_left_coord[0], far_left_coord[1]
 
-    def max_coord(self, coords):
-        max_coord = []
+    def farthest_coord(self, coords):
+        farthest_distance = 0
+        farthest_coord = []
+
         for coord in coords:
-            if len(max_coord) == 0:
-                max_coord = coord[0]
+            if farthest_distance == 0:
+                farthest_distance = self.distance_from_origin(coord[0])
+                farthest_coord = coord[0]
             else:
-                if (coord[0][0] >= max_coord[0] 
-                    and coord[0][1] >= max_coord[1]):
+                cur_distance = self.distance_from_origin(coord[0])
+                if cur_distance >= farthest_distance:
+                    farthest_distance = cur_distance
+                    farthest_coord = coord[0]
 
-                    max_coord = coord[0]
+        return farthest_coord[0], farthest_coord[1]     
 
-        return max_coord[0], max_coord[1]     
+    def distance_from_origin(self, coord):
+        return math.sqrt(coord[0]*2 + coord[1]*2)
 
     def __init__(self):
     	pass
